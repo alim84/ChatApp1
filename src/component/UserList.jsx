35c2c1c926077c926b53ddaeaa954191e9d43abe
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-09-11 12:26:09 +0600

    Friend Request

--- src/component/UserList.jsx
+++ src/component/UserList.jsx
@@ -1,6 +1,6 @@
 import { BsThreeDotsVertical } from "react-icons/bs";
 import profileImg from "../assets/alim.png";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, push } from "firebase/database";
 import { useEffect, useState } from "react";
 import moment from "moment/moment";
 import { useSelector } from "react-redux";
@@ -10,6 +10,7 @@ const UserList = () => {
   let data = useSelector((state) => state.userInfo.value);
   const db = getDatabase();
   let [userList, setUserList] = useState([]);
+  let [requestList, setrequestList] = useState([]);
 
   useEffect(() => {
     const userListRef = ref(db, "users/");
@@ -17,48 +18,82 @@ const UserList = () => {
       let array = [];
       snapshot.forEach((item) => {
         if (data.uid != item.key) {
-          array.push(item.val());
+          array.push({ ...item.val(), uid: item.key });
         }
       });
       setUserList(array);
     });
   }, []);
+  useEffect(() => {
+    const friendrequestRef = ref(db, "friendrequest/");
+    onValue(friendrequestRef, (snapshot) => {
+      let array = [];
+      snapshot.forEach((item) => {
+        array.push(item.val().senderid + item.val().receiverid);
+      });
+      setrequestList(array);
+    });
+  }, []);
 
+  let handleFriendReq = (item) => {
+    set(push(ref(db, "friendrequest/")), {
+      senderid: data.uid,
+      sendername: data.displayName,
+      senderemail: data.email,
+      receiverid: item.uid,
+      receivername: item.name,
+      receiveremail: item.email,
+      date: `${new Date().getFullYear()}-${
+        new Date().getMonth() + 1
+      }-${new Date().getDate()}-${new Date().getHours()}-${new Date().getMinutes()}-${new Date().getSeconds()}`,
+    }).then(() => {
+      alert("Your request success");
+    });
+  };
   return (
-      <div className="w-full h-[347px] rounded-2xl  overflow-scroll">
-    <div className="w-[427px] shadow-2xl rounded-2xl px-5 ">
-      <div className="flex justify-between items-center">
-        <h2 className="text-[20px] font-semibold text-black">User List </h2>
-        <BsThreeDotsVertical />
+    <div className="w-full h-[347px] rounded-2xl  overflow-scroll">
+      <div className="w-[427px] shadow-2xl rounded-2xl px-5 ">
+        <div className="flex justify-between items-center">
+          <h2 className="text-[20px] font-semibold text-black">User List </h2>
+          <BsThreeDotsVertical />
+        </div>
       </div>
-      </div>
-     
+
       {userList.map((item) => (
-        
         <div className="flex justify-between items-center mt-[17px] border-b border-black/25 pb-6">
-        <div className="flex items-center gap-4">
-              <img
-                className="w-[70px] h-[70px] rounded-full"
-                src={item ? item.image : SigninImage }
-                alt=""
-                />
-              <div>
-                <h3 className="text-[18px] font-semibold text-black">
-                  {item.name}
-                </h3>
-                <p className="text-[14px] font-semibold text-gray-500">
-                  {moment(item.date, "YYYYMMDD").fromNow()}
-                </p>
-              </div>
+          <div className="flex items-center gap-4">
+            <img
+              className="w-[70px] h-[70px] rounded-full"
+              src={item ? item.image : SigninImage}
+              alt=""
+            />
+            <div>
+              <h3 className="text-[18px] font-semibold text-black">
+                {item.name}
+              </h3>
+              <p className="text-[14px] font-semibold text-gray-500">
+                {moment(item.date, "YYYYMMDD").fromNow()}
+              </p>
             </div>
+          </div>
+          {requestList.includes(data.uid + item.uid) ||
+          requestList.includes(item.uid + data.uid) ? (
             <button className="bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg">
+              {" "}
+              -
+            </button>
+          ) : (
+            <button
+              onClick={() => handleFriendReq(item)}
+              className="bg-primary px-5 py-2 text-white font-normal text-[18px] rounded-lg"
+            >
               {" "}
               +
             </button>
-              </div>
-          
-        ) )}
+          )}
         </div>
+      ))}
+    </div>
   );
 };
 
