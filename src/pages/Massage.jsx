d0642b9167a415527648ef5facfc5044d5e0c108
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2024-10-02 16:28:14 +0600

    Message Part Uddate

--- src/pages/Massage.jsx
+++ src/pages/Massage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FriendList from "./../component/FriendList";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import imgage from "../assets/alim.png";
@@ -8,8 +8,17 @@ import { TbPhotoFilled } from "react-icons/tb";
 import { FaMicrophone } from "react-icons/fa6";
 import { GrLike } from "react-icons/gr";
 import { MdEmojiEmotions } from "react-icons/md";
+import { useSelector } from "react-redux";
 
 const Massage = () => {
+  let chatdata = useSelector((state) => state.chatuserInfo.value);
+  let data = useSelector((state) => state.userInfo.value);
+  let [msgtext, setmsgtext] = useState("");
+  let handlemsgInpur = (e) => {
+    setmsgtext(e.target.value);
+  };
+
+  let handlemsgSubmit = () => {};
   return (
     <>
       <div className="grid grid-cols-2 w-full justify-between h-[900px] mt-[50px] ">
@@ -24,12 +33,10 @@ const Massage = () => {
               </div>
 
               <div className=" text-blue-900 font-bold ">
-                <h2 className="text-[14px]">Alim</h2>
+                <h2 className="text-[14px]">{chatdata && chatdata.name}</h2>
                 <p className="text-[8px]">Online</p>
               </div>
             </div>
-
-           
           </div>
           <div className="w-full h-[800px] bg-blue-50 rounded-md relative ">
             <div className="w-[300px] bg-blue-900 text-white px-2 rounded-lg py-3  absolute translate-x-0 translate-y-[50px] ">
@@ -55,13 +62,17 @@ const Massage = () => {
             </div>
             <div className="w-full bg-blue-100 ">
               <input
+                onChange={handlemsgInpur}
                 className="w-full py-3 outline-none  px-3 rounded-full "
                 placeholder="Message type............"
                 type="text"
               ></input>
-          <button className="absolute translate-x-[-40px] translate-y-[10px]">   <MdEmojiEmotions className=" text-[25px] text-yellow-600 "/></button>
+              <button className="absolute translate-x-[-40px] translate-y-[10px]">
+                {" "}
+                <MdEmojiEmotions className=" text-[25px] text-yellow-600 " />
+              </button>
             </div>
-            <button className=" bg-blue-100 py-3  ">
+            <button onClick={handlemsgSubmit} className=" bg-blue-100 py-3  ">
               {" "}
               <IoIosSend className="text-purple-800 w-[25px] h-[25px] ml-2 " />
             </button>
